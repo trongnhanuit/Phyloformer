4fbe1bb76a5d8d527c12c546f610772f87578662
NhanLT <trongnhan.uit@gmail.com>
2023-07-20 17:11:48 +1000

    parallelize making tensors; update requirements.txt for Gadi

--- phyloformer/__init__.py
+++ phyloformer/__init__.py
@@ -4,4 +4,4 @@ import phyloformer
 import phyloformer.attentions
 
 __title__ = __name__
-__version__ = metadata.version(__title__)
+#__version__ = metadata.version(__title__)
--- phyloformer/scripts/make_tensors.py
+++ phyloformer/scripts/make_tensors.py
@@ -3,15 +3,25 @@ import os
 
 import torch
 from tqdm import tqdm
+from multiprocessing import Pool
+from functools import partial
+
+# for linux
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+#print(sys.path)
 
 from phyloformer.data import load_alignment, load_tree
 
+def process_a_tree(tree_file, tree_dir: str, aln_dir: str, out_dir: str):
+    identifier = tree_file.rstrip(".nwk")
+    filename = os.path.join(out_dir, f"{identifier}.tensor_pair")
 
-def make_tensors(tree_dir: str, aln_dir: str, out_dir: str):
-    trees = [file for file in os.listdir(tree_dir) if file.endswith(".nwk")]
-    for tree_file in (pbar := tqdm(trees)):
-        identifier = tree_file.rstrip(".nwk")
-        pbar.set_description(f"Processing {identifier}")
+    # if the file exists -> ignore
+    if (not (os.path.isfile(filename) and os.path.getsize(filename) > 0)):
+        #pbar.set_description(f"Processing {identifier}")
         tree_tensor, _ = load_tree(os.path.join(tree_dir, tree_file))
         aln_tensor, _ = load_alignment(os.path.join(aln_dir, f"{identifier}.fasta"))
 
@@ -19,6 +29,14 @@ def make_tensors(tree_dir: str, aln_dir: str, out_dir: str):
             {"X": aln_tensor, "y": tree_tensor},
             os.path.join(out_dir, f"{identifier}.tensor_pair"),
         )
+
+def make_tensors(tree_dir: str, aln_dir: str, out_dir: str, nprocesses):
+    trees = [file for file in os.listdir(tree_dir) if file.endswith(".nwk")]
+
+    pool = Pool(nprocesses)                         # Create a multiprocessing Pool
+    with tqdm(total=len(trees)) as pbar:
+        for _iter in pool.imap_unordered(partial(process_a_tree, tree_dir=tree_dir, aln_dir=aln_dir, out_dir=out_dir), trees):
+            pbar.update()
 
 
 def main():
@@ -47,12 +65,13 @@ def main():
         type=str,
         help="path to output directory (default: current directory)",
     )
+    parser.add_argument('-p', '--nprocesses', type=int, required=True, help='number of processes (default:1)', default=1)
     args = parser.parse_args()
 
     if not os.path.exists(args.output):
         os.mkdir(args.output)
 
-    make_tensors(args.treedir, args.alidir, args.output)
+    make_tensors(args.treedir, args.alidir, args.output, args.nprocesses)
 
 
 if __name__ == "__main__":
