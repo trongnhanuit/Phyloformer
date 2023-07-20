3287a830f59195db2e0f32055cb94dfe460b890f
NhanLT <trongnhan.uit@gmail.com>
2023-07-20 11:24:20 +1000

    Parallelize the simulation of trees

--- phyloformer/scripts/simulateTrees.py
+++ phyloformer/scripts/simulateTrees.py
@@ -5,43 +5,49 @@ import numpy as np
 from dendropy.simulate import treesim
 from ete3 import Tree
 from tqdm import tqdm
+from multiprocessing import Pool
+from functools import partial
 
+def simulate_a_tree(tree_id, numleaves, outdir, treeType, bl):
+    # Generating the tree topology
+    outname = ""
+    outname = os.path.join(outdir, str(tree_id) + "_" + str(numleaves) + "_leaves.nwk")
+    if treeType == "birth-death":  # using dendropy
+        t = treesim.birth_death_tree(
+            birth_rate=1.0, death_rate=0.5, num_extant_tips=numleaves
+        )
+        t.write(path=outname, schema="newick", suppress_rooting=True)
+    elif treeType == "uniform":  # using ete3
+        t = Tree()
+        t.populate(numleaves)
+        t.write(format=1, outfile=outname)
+    else:
+        exit("Error, treetype should be birth-death or uniform")
+    t = Tree(outname)
 
-def simulate_trees(numtrees, numleaves, outdir, treeType, bl):
+    # Assigning the branch lengths
+    for node in t.traverse("postorder"):
+        if node.is_root():
+            pass
+        else:
+            if bl == "uniform":
+                node.dist = np.random.uniform(low=0.002, high=1.0, size=None)
+            elif bl == "exponential":
+                node.dist = np.random.exponential(0.15, size=None)
+            else:
+                exit(
+                    "Error, branch length distribution should be uniform or exponential"
+                )
+    t.write(format=1, outfile=outname)
+
+def simulate_trees(numtrees, numleaves, outdir, treeType, bl, nprocesses):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
-    for i in tqdm(range(numtrees)):
-        # Generating the tree topology
-        outname = ""
-        outname = os.path.join(outdir, str(i) + "_" + str(numleaves) + "_leaves.nwk")
-        if treeType == "birth-death":  # using dendropy
-            t = treesim.birth_death_tree(
-                birth_rate=1.0, death_rate=0.5, num_extant_tips=numleaves
-            )
-            t.write(path=outname, schema="newick", suppress_rooting=True)
-        elif treeType == "uniform":  # using ete3
-            t = Tree()
-            t.populate(numleaves)
-            t.write(format=1, outfile=outname)
-        else:
-            exit("Error, treetype should be birth-death or uniform")
-        t = Tree(outname)
-
-        # Assigning the branch lengths
-        for node in t.traverse("postorder"):
-            if node.is_root():
-                pass
-            else:
-                if bl == "uniform":
-                    node.dist = np.random.uniform(low=0.002, high=1.0, size=None)
-                elif bl == "exponential":
-                    node.dist = np.random.exponential(0.15, size=None)
-                else:
-                    exit(
-                        "Error, branch length distribution should be uniform or exponential"
-                    )
-        t.write(format=1, outfile=outname)
+    pool = Pool(nprocesses)                         # Create a multiprocessing Pool
+    with tqdm(total=numtrees) as pbar:
+        for _ in pool.imap_unordered(partial(simulate_a_tree, numleaves = numleaves, outdir = outdir, treeType = treeType, bl = bl), range(numtrees)):
+            pbar.update()
 
     with open(os.path.join(outdir, "stdout.txt"), "a") as fout:
         fout.write(
@@ -91,10 +97,11 @@ def main():
         choices=BRLENS,
         metavar="BL",
     )
+    parser.add_argument('-p', '--nprocesses', type=int, required=True, help='number of processes (default:1)', default=1)
     args = parser.parse_args()
 
     simulate_trees(
-        args.ntrees, args.nleaves, args.output, args.topology, args.branchlength
+        args.ntrees, args.nleaves, args.output, args.topology, args.branchlength, args.nprocesses
     )
 
 
